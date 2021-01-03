78cff17f15cca2eff742afcddbaccb26d5b40265
Farruch Kouliev <farruch.kouliev@gmail.com>
2021-01-03 13:41:50 +0100

    MOD-61 Created USP in WebPage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
