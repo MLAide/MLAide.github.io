3754ba5d3ad48dd882ecfe2c54eea8f99137c6e8
Farruch Kouliev <farruch.kouliev@gmail.com>
2021-03-07 17:04:58 +0100

    MOD-61 Added Links to Docs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,30 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import * as FontAwesomeBrands from '@fortawesome/free-brands-svg-icons';
-import { NgcCookieConsentService, NgcInitializeEvent, NgcNoCookieLawEvent, NgcStatusChangeEvent } from 'ngx-cookieconsent';
-import { Subscription } from 'rxjs';
+import {
+  Component,
+  Inject,
+  Injector,
+  OnDestroy,
+  OnInit,
+  PLATFORM_ID,
+} from "@angular/core";
+import * as FontAwesomeBrands from "@fortawesome/free-brands-svg-icons";
+import {
+  NgcCookieConsentService,
+  NgcInitializeEvent,
+  NgcNoCookieLawEvent,
+  NgcStatusChangeEvent,
+} from "ngx-cookieconsent";
+import { Subscription } from "rxjs";
+import { isPlatformBrowser, isPlatformServer } from "@angular/common";
+
+type NewType = Injector;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent implements OnInit, OnDestroy {
-  title = 'webpage';
   faGithub = FontAwesomeBrands.faGithub;
-  faSlack = FontAwesomeBrands.faSlack;
-  faTwitter = FontAwesomeBrands.faTwitter;
 
   //keep refs to subscriptions to be able to unsubscribe later
   private popupOpenSubscription: Subscription;
@@ -22,48 +34,74 @@ export class AppComponent implements OnInit, OnDestroy {
   private revokeChoiceSubscription: Subscription;
   private noCookieLawSubscription: Subscription;
 
-  constructor(private ccService: NgcCookieConsentService) { }
+  constructor(
+    private ccService: NgcCookieConsentService,
+    private readonly injector: Injector,
+    @Inject(PLATFORM_ID) private platformId
+  ) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.injector.get(NgcCookieConsentService);
+    }
+  }
 
+  //TODO: https://www.positronx.io/angular-seo-set-dynamic-page-title-meta-tags-in-universal-app/
   ngOnInit() {
-    // subscribe to cookieconsent observables to react to main events
-    this.popupOpenSubscription = this.ccService.popupOpen$.subscribe(
-      () => {
+    if (isPlatformBrowser(this.platformId)) {
+      // subscribe to cookieconsent observables to react to main events
+      this.popupOpenSubscription = this.ccService.popupOpen$.subscribe(() => {
         // you can use this.ccService.getConfig() to do stuff...
       });
 
-    this.popupCloseSubscription = this.ccService.popupClose$.subscribe(
-      () => {
+      this.popupCloseSubscription = this.ccService.popupClose$.subscribe(() => {
         // you can use this.ccService.getConfig() to do stuff...
       });
 
-    this.initializeSubscription = this.ccService.initialize$.subscribe(
-      (event: NgcInitializeEvent) => {
-        // you can use this.ccService.getConfig() to do stuff...
-      });
+      this.initializeSubscription = this.ccService.initialize$.subscribe(
+        (event: NgcInitializeEvent) => {
+          // you can use this.ccService.getConfig() to do stuff...
+        }
+      );
 
-    this.statusChangeSubscription = this.ccService.statusChange$.subscribe(
-      (event: NgcStatusChangeEvent) => {
-        // you can use this.ccService.getConfig() to do stuff...
-      });
+      this.statusChangeSubscription = this.ccService.statusChange$.subscribe(
+        (event: NgcStatusChangeEvent) => {
+          // you can use this.ccService.getConfig() to do stuff...
+        }
+      );
 
-    this.revokeChoiceSubscription = this.ccService.revokeChoice$.subscribe(
-      () => {
-        // you can use this.ccService.getConfig() to do stuff...
-      });
+      this.revokeChoiceSubscription = this.ccService.revokeChoice$.subscribe(
+        () => {
+          // you can use this.ccService.getConfig() to do stuff...
+        }
+      );
 
-    this.noCookieLawSubscription = this.ccService.noCookieLaw$.subscribe(
-      (event: NgcNoCookieLawEvent) => {
-        // you can use this.ccService.getConfig() to do stuff...
-      });
+      this.noCookieLawSubscription = this.ccService.noCookieLaw$.subscribe(
+        (event: NgcNoCookieLawEvent) => {
+          // you can use this.ccService.getConfig() to do stuff...
+        }
+      );
+    }
   }
 
   ngOnDestroy() {
     // unsubscribe to cookieconsent observables to prevent memory leaks
-    this.popupOpenSubscription.unsubscribe();
-    this.popupCloseSubscription.unsubscribe();
-    this.initializeSubscription.unsubscribe();
-    this.statusChangeSubscription.unsubscribe();
-    this.revokeChoiceSubscription.unsubscribe();
-    this.noCookieLawSubscription.unsubscribe();
+    if (this.popupOpenSubscription) {
+      this.popupOpenSubscription.unsubscribe();
+    }
+
+    if (this.popupCloseSubscription) {
+      this.popupCloseSubscription.unsubscribe();
+    }
+    if (this.initializeSubscription) {
+      this.initializeSubscription.unsubscribe();
+    }
+    if (this.statusChangeSubscription) {
+      this.statusChangeSubscription.unsubscribe();
+    }
+    if (this.revokeChoiceSubscription) {
+      this.revokeChoiceSubscription.unsubscribe();
+    }
+    if (this.noCookieLawSubscription) {
+      this.noCookieLawSubscription.unsubscribe();
+    }
   }
 }
