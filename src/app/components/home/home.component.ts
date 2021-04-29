367634f607e4db518629a1c4a9909bd106759f1a
Farruch Kouliev <farruch.kouliev@gmail.com>
2021-04-29 14:49:07 +0200

    MOD-129 Updated logo, favicon, links and contact email

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,12 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   public keyFeatures = [
-    { title: "Experiment Tracking", content: "Track parameters, metrics and artifacts in your machine learning experiments that are organized by single runs.", imgUrl: "../../../assets/Home/tracking.png" },
-    { title: "Artifact Management", content: "Attach artifacts like code, configs or models to your experiment runs and reuse them in your next run.", imgUrl: "../../../assets/Home/artifact-management.png" },
+    { title: "Experiment Tracking", content: "Track parameters, metrics, and artifacts in your machine learning experiments that are organized by single runs.", imgUrl: "../../../assets/Home/tracking.png" },
+    { title: "Artifact Management", content: "Attach artifacts like code, configs, or models to your experiment runs and reuse them in your next run.", imgUrl: "../../../assets/Home/artifact-management.png" },
     { title: "Experiment Lineage", content: "Inspect your experiment with a visualized lineage representing the relationship between all runs and artifacts.", imgUrl: "../../../assets/Home/lineage.png" },
     { title: "Model Staging", content: "Put your models under version control and stage them to obtain transparency and reproducibility in your operations.", imgUrl: "../../../assets/Home/model-management.png" },
     { title: "Run Evaluation", content: "Evaluate your runs by viewing or comparing parameters and metrics to identify the best model for your machine learning product.", imgUrl: "../../../assets/Home/run-comparison.png" },
-    { title: "ML Library Integration", content: "Raising amount of integrated machine learning libraries for convenient tracking of parameters, metrics and models.", imgUrl: "../../../assets/Home/library-integration.png" },
+    { title: "ML Library Integration", content: "An increasing number of integrated machine learning libraries for convenient in-code tracking of parameters, metrics, and models.", imgUrl: "../../../assets/Home/library-integration.png" },
     { title: "Access Management", content: "Manage access to your machine learning projects and collaborate with other members of your team.", imgUrl: "../../../assets/Home/project-members.png" },
   ];
 
