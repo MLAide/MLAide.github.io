b1b04eb631e51e3545f7d1846ce4f58a700e4d97
Farruch Kouliev <farruch.kouliev@gmail.com>
2021-11-01 21:38:10 +0100

    MOD-193 Added integration info of ml frameworks on web page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,13 +7,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   public keyFeatures = [
-    { title: "Experiment Tracking", content: "Track parameters, metrics, and artifacts in your machine learning experiments that are organized by single runs.", imgUrl: "../../../assets/Home/experiment-tracking.png", altMessage: "ML Aide web user interface - run details view" },
-    { title: "Artifact Management", content: "Attach artifacts like code, configs, or models to your experiment runs and reuse them in your next run.", imgUrl: "../../../assets/Home/artifact-management.png", altMessage: "ML Aide web user interface - artifacts list view" },
-    { title: "Experiment Lineage", content: "Inspect your experiment with a visualized lineage representing the relationship between all runs and artifacts.", imgUrl: "../../../assets/Home/experiment-lineage.png", altMessage: "ML Aide web user interface - experiment details view" },
-    { title: "Model Staging", content: "Put your models under version control and stage them to obtain transparency and reproducibility in your operations.", imgUrl: "../../../assets/Home/model-management.png", altMessage: "ML Aide web user interface - model management view" },
-    { title: "Run Evaluation", content: "Evaluate your runs by viewing or comparing parameters and metrics to identify the best model for your machine learning product.", imgUrl: "../../../assets/Home/run-comparison.png", altMessage: "ML Aide web user interface - run comparison view" },
-    { title: "ML Library Integration", content: "An increasing number of integrated machine learning libraries for convenient in-code tracking of parameters, metrics, and models.", imgUrl: "../../../assets/Home/library-integration.png", altMessage: "Jupyter notebook example with ML Aide's python client integration" },
-    { title: "Access Management", content: "Manage access to your machine learning projects and collaborate with other members of your team.", imgUrl: "../../../assets/Home/access-management.png", altMessage: "ML Aide web user interface - access management view" },
+    {
+      title: "Experiment Tracking",
+      content: "Track parameters, metrics, and artifacts in your machine learning experiments that are organized by single runs.",
+      imgUrl: "../../../assets/Home/experiment-tracking.png",
+      altMessage: "ML Aide web user interface - run details view"
+    },
+    {
+      title: "Artifact Management",
+      content: "Attach artifacts like code, configs, or models to your experiment runs and reuse them in your next run.",
+      imgUrl: "../../../assets/Home/artifact-management.png",
+      altMessage: "ML Aide web user interface - artifacts list view"
+    },
+    {
+      title: "Experiment Lineage",
+      content: "Inspect your experiment with a visualized lineage representing the relationship between all runs and artifacts.",
+      imgUrl: "../../../assets/Home/experiment-lineage.png",
+      altMessage: "ML Aide web user interface - experiment details view"
+    },
+    {
+      title: "Model Staging",
+      content: "Put your models under version control and stage them to obtain transparency and reproducibility in your operations.",
+      imgUrl: "../../../assets/Home/model-management.png",
+      altMessage: "ML Aide web user interface - model management view" },
+    {
+      title: "Run Evaluation",
+      content: "Evaluate your runs by viewing or comparing parameters and metrics to identify the best model for your machine learning product.",
+      imgUrl: "../../../assets/Home/run-comparison.png",
+      altMessage: "ML Aide web user interface - run comparison view"
+    },
+    {
+      title: "ML Library Integration",
+      content: "An increasing number of integrated machine learning libraries for convenient in-code tracking of parameters, metrics, and models.",
+      imgUrl: "../../../assets/Home/library-integration.png",
+      altMessage: "Jupyter notebook example with ML Aide's python client integration" },
+    {
+      title: "Access Management",
+      content: "Manage access to your machine learning projects and collaborate with other members of your team.",
+      imgUrl: "../../../assets/Home/access-management.png",
+      altMessage: "ML Aide web user interface - access management view"
+    },
+  ];
+
+  public mlFrameworks = [
+    {
+      altText: "Tensorflow",
+      imgUrl: "../../../assets/Home/tensorflow.png",
+    },
+    {
+      altText: "PyTorch",
+      imgUrl: "../../../assets/Home/pytorch.png",
+    },
+    {
+      altText: "Scikit learn",
+      imgUrl: "../../../assets/Home/scikit-learn.png",
+    }
   ];
 
   public roadmap = [
